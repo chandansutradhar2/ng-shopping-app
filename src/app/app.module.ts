6d952504607dcaba50f3af898455a5d392eb3d56
chandan_naresh <chandansutradhar2@gmail.com>
2022-04-22 11:54:52 +0530

    header component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { UiModule } from './ui/ui.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
@@ -21,7 +25,7 @@ import { NavbarComponent } from './layouts/header/navbar/navbar.component';
     SearchBarComponent,
     IconBarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, UiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
